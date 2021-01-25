d0793d3fe7002533c625632f62882c410c6ba4ed
Avi <avi.gep@gmail.com>
2021-01-26 01:52:39 +0530

    add validation for thought contents

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,5 +1,6 @@
 class Thought < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
+  validates :contents, presence: true, length: { minimum: 10 }
 
   self.per_page = 10
 end
