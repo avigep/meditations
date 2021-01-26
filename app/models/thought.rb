5b8a0e78410318dc874785b491936e4dd7dfa886
Avi <avi.gep@gmail.com>
2021-01-26 20:51:53 +0530

    add annotate gem

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: thoughts
+#
+#  id           :bigint           not null, primary key
+#  contents     :text(65535)
+#  publish_date :date
+#  read_time    :integer
+#  title        :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Thought < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
   validates :contents, presence: true, length: { minimum: 10 }
